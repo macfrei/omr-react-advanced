4f07a4e23c4f64ec02390b84acbe4f773603eb83
macfrei <lene@7freiheiten.de>
2021-02-25 14:22:39 +0100

    Add form

---
+++ src/components/AppForm.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components/macro'
+
+export default function AppForm({ onAddTodo }) {
+  return (
+    <Form onSubmit={handleSubmit}>
+      <label>
+        Todo:
+        <input name="todo" placeholder="Todo?" />
+      </label>
+    </Form>
+  )
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const form = event.target
+    const input = form.elements.todo
+    const todo = input.value
+    onAddTodo(todo)
+    form.reset()
+    input.focus()
+  }
+}
+
+const Form = styled.form`
+  display: grid;
+  gap: 10px;
+
+  label {
+    display: grid;
+    gap: 4px;
+  }
+  input {
+    border: 2px solid #bbb;
+  }
+`
